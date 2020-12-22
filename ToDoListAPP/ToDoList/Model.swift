32e94a64b685612c91668949806f0f8bda74c3ca
eldaroid <eldaroid@MacBook-Air-Eldar.local>
2020-12-22 23:59:32 +0300

    add todoList

--- ToDoListAPP/ToDoList/Model.swift
+++ ToDoListAPP/ToDoList/Model.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-var toDoList: [[String: Any]] = [["Name" : "Позвонить маме1", "isComplited" : false], ["Name" : "Позвонить маме2", "isComplited" : false], ["Name" : "Позвонить маме", "isComplited" : true]]
+var toDoList: [[String: Any]] = [["Name" : "Позвонить маме1", "isComplited" : false], ["Name" : "Позвонить маме2", "isComplited" : false], ["Name" : "Позвонить маме3", "isComplited" : true], ["Name" : "Позвонить маме4", "isComplited" : false]]
 
 func addItem(nameItem: String, isCompleted: Bool = false) {
     toDoList.append(["Name" : nameItem, "isComplited" : isCompleted])
